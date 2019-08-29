ac3911411d02e9d7eda9c21415eee1588ef3606b
MrJQA <MrJacklin@ukr.net>
2019-08-29 14:33:39 +0300

    Home task 1 & 2

---
+++ src/main/java/Lesson2.java
@@ -0,0 +1,51 @@
+import java.util.Arrays;
+
+public class Lesson2 {
+
+
+
+    public static void main(String[] args)
+    {
+        int[] myArray = {1,2,3,4,5,3,2,5,7,9,0,2,3,4,1};
+        int sumArray = 0;
+        //Task 1 Option 1
+        sumArray = Arrays.stream(myArray).sum();
+        System.out.println(sumArray);
+
+        //Task 1 Option 2
+        sumArray = 0;
+        for(int i = 0; i < myArray.length; i++)
+        {
+            sumArray += myArray[i];
+        }
+        System.out.println(sumArray);
+
+        //Task 1 Option 3
+        sumArray = 0;
+        for(Integer i:myArray)
+        {
+            sumArray += i;
+        }
+        System.out.println(sumArray);
+
+        //Task 2 Option 1
+        boolean flag = true;
+        while (flag)
+        {
+            flag = false;
+            for(int i = 1; i < myArray.length; i++)
+            {
+                if ( ((myArray[i-1] % 2) != 0)  && ((myArray[i] % 2) == 0))
+                {
+                    flag = true;
+                    int swap = myArray[i-1];
+                    myArray[i-1] = myArray[i];
+                    myArray[i] = swap;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(myArray));
+
+    }
+
+}
