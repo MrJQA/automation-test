2fab83b77a48df8d9403a3088ec7d399c2fed077
MrJQA <MrJacklin@ukr.net>
2019-09-10 12:47:52 +0300

    input iutput

--- src/test/java/test/automation/Lesson2ArrayTransform.java
+++ src/test/java/test/automation/Lesson2ArrayTransform.java
@@ -1,15 +1,14 @@
 package test.automation;
 
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 import java.util.Arrays;
 
 public class Lesson2ArrayTransform {
 
-    int[] inputArray = {1,2,3,4,5,3,2,5,7,9,0,2,3,4,1};
-    int[] outputArray = {2,4,2,0,2,4,1,3,5,3,5,7,9,3,1};
+    int[] inputArray = {1,2,3,4,5,-3,2,5,7,9,0,2,3,4,1,-10,12,14};
+    int[] outputArray = {2,4,2,0,2,4,-10,12,14,1,3,5,-3,5,7,9,3,1};
 
     @Test
     public void arrayTransformTest()
