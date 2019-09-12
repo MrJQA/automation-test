a49179a3e27fdc830fe63510d6d1ca6f9dbea34c
MrJQA <MrJacklin@ukr.net>
2019-09-12 17:05:11 +0300

    input iutput

--- src/test/java/test/automation/Leson3Test.java
+++ src/test/java/test/automation/Leson3Test.java
@@ -1,5 +1,6 @@
 package test.automation;
 
+import test.automation.transforms.ArrayTransformSort;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import java.util.Arrays;
@@ -10,10 +11,16 @@ public class Leson3Test {
     int[] inputArray = {1,2,3,4,5,-3,2,5,7,9,0,2,3,4,1,-10,12,14};
     int[] outputArray = {2,4,2,0,2,4,-10,12,14,1,3,5,-3,5,7,9,3,1};
 
+    ArrayTransformSort at = new ArrayTransformSort();
+
+
     @Test
     public void arrayTransformTest()
     {
+        System.out.println(Arrays.toString(inputArray));
+        System.out.println(Arrays.toString(at.transform(inputArray)));
+        assertEquals(Arrays.toString(at.transform(inputArray)), Arrays.toString(outputArray));
         //assertTrue(Arrays.equals(inputArray, outputArray));
-        assertEquals(Arrays.toString(inputArray), Arrays.toString(outputArray));
+        // assertEquals(Arrays.toString(inputArray), Arrays.toString(outputArray));
     }
 }
