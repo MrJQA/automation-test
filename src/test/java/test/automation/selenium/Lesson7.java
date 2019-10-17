f998fae7c501914a3622c2b004c3be4d4984215f
MrJQA <MrJacklin@ukr.net>
2019-10-17 16:44:40 +0300

    Lesson 7 task 2

--- src/test/java/test/automation/selenium/Lesson7.java
+++ src/test/java/test/automation/selenium/Lesson7.java
@@ -7,7 +7,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.Arrays;
 import java.util.List;
+import static junit.framework.TestCase.*;
 
 public class Lesson7 {
 String ttt = "Українська";
@@ -36,7 +38,7 @@ String ttt = "Українська";
         List<WebElement> langsLong = header.findElements(By.className("header__lang-long-name"));
         List<WebElement> langsShort = header.findElements(By.className("header__lang-short-name"));
         //Українська
-        if (("Українська".equals(langsLong.get(0).getText()) || "Укр".equals(langsLong.get(0).getText())) &&
+        /*if (("Українська".equals(langsLong.get(0).getText()) || "Укр".equals(langsLong.get(0).getText())) &&
             (langsLong.get(0).isDisplayed() || langsShort.get(0).isDisplayed()))
         { System.out.println("Українська мова присутня");}
         else
@@ -52,10 +54,27 @@ String ttt = "Українська";
                 (langsLong.get(2).isDisplayed() || langsShort.get(2).isDisplayed()))
         { System.out.println("English мова присутня");}
         else
-        { System.out.println("Test 1. халепа з English мова");}
-        System.out.println("Test1 ---кінець---");
+        { System.out.println("Test 1. халепа з English мова");}*/
 
+        assertTrue("\n Test 1. халепа з Українська мова",("Українська".equals(langsLong.get(0).getText()) || "Укр".equals(langsLong.get(0).getText())) &&
+                (langsLong.get(0).isDisplayed() || langsShort.get(0).isDisplayed()));
+        assertTrue("\n Test 1. халепа з Російська мова", ("Русский".equals(langsLong.get(1).getText()) || "Рус".equals(langsLong.get(1).getText())) &&
+                (langsLong.get(1).isDisplayed() || langsShort.get(1).isDisplayed()));
+        assertTrue("\n Test 1. халепа з English мова", ("English".equals(langsLong.get(2).getText()) || "Eng".equals(langsLong.get(2).getText())) &&
+                (langsLong.get(2).isDisplayed() || langsShort.get(2).isDisplayed()));
+        System.out.println("Test1 ---кінець---");
     }
 
+    @Test
+    public void tets2()
+    {
+        System.out.println("Test2 ---Початок---");
+        driver.navigate().to("https://accounts.ukr.net/registration");
+        WebElement activLang = driver.findElement(By.className("header__lang-item_active"));
+        List<WebElement> noActivLang = driver.findElements(By.className("header__lang-item"));
+        assertTrue("\n Test 2. Колір активної не корректний", "rgba(52, 56, 64, 1)".equals(activLang.getCssValue("color")));
+        assertTrue("\n Test 2. Колір не активних мов не корректний", "rgba(102, 153, 0, 1)".equals(noActivLang.get(1).getCssValue("color")) && "rgba(102, 153, 0, 1)".equals(noActivLang.get(1).getCssValue("color")) );
 
+        System.out.println("Test2 ---кінець---");
+    }
 }
